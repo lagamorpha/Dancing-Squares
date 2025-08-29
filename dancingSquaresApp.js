bbd87418ac61d7434d4e0aa35a72944d5af9aa47
Michael Chandler <macjr93@yahoo.com>
2025-08-28 18:55:29 -0700

    add comments to isolate yellow box for move function

--- dancingSquaresApp.js
+++ dancingSquaresApp.js
@@ -2,8 +2,8 @@
 
 
 // Variable Block
-const blackBox = document.querySelector('.square');
-// const yellowBox = document.querySelector('.sensor');
+// const blackBox = document.querySelector('.square');
+const yellowBox = document.querySelector('.sensor');
 
 // Basic Functions
 const makeRandColor = () => {
@@ -13,12 +13,12 @@ const makeRandColor = () => {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
-// blackBox Functions
-blackBox.addEventListener('click', function () {
-    const newColor = makeRandColor();
-    blackBox.style.backgroundColor = newColor;
-    // console.log(`The Black Box is now ${newColor}`);
-});
+// // blackBox Functions
+// blackBox.addEventListener('click', function () {
+//     const newColor = makeRandColor();
+//     blackBox.style.backgroundColor = newColor;
+//     console.log(`The Black Box is now ${newColor}`);
+// });
 
 // yellowBox Functions
 // yellowBox.addEventListener('click', function () {
@@ -26,71 +26,30 @@ blackBox.addEventListener('click', function () {
 // })
 
 // 'move the box' functions
-document.addEventListener('keydown', function (event) {
-    const yellowBox = document.querySelector('.sensor');
-    let currentTop = parseInt(yellowBox.style.top);
-    let currentLeft = parseInt(yellowBox.style.left);
-    const moveAmount = 10; // Pixels to move on each keypress
+document.addEventListener(
+    "keydown",
+    (event) => {
+        const keyName = event.key;
+        let currentTop = parseInt(yellowBox.style.top);
+        let currentLeft = parseInt(yellowBox.style.left);
+        const moveAmount = 10;
 
-    switch (event.key) {
-        case 'ArrowUp':
-            yellowBox.style.top = (currentTop - moveAmount) + 'px';
-            event.preventDefault(); // Prevent default scroll behavior
-            break;
-        case 'ArrowDown':
+        if (keyName === 'w') {
             yellowBox.style.top = (currentTop + moveAmount) + 'px';
-            event.preventDefault();
-            break;
-        case 'ArrowLeft':
+            console.log('you moved forwards');
+        }
+        if (keyName === 'a') {
             yellowBox.style.left = (currentLeft - moveAmount) + 'px';
-            event.preventDefault();
-            break;
-        case 'ArrowRight':
+            console.log('you moved left');
+        }
+        if (keyName === 'd') {
             yellowBox.style.left = (currentLeft + moveAmount) + 'px';
-            event.preventDefault();
-            break;
-    }
-});
-
-// document.addEventListener(
-//     "keydown",
-//     (event) => {
-//         const keyName = event.key;
-
-//         if (keyName === 'w') {
-//             yellowBox.style.postion = relative + '25px';
-//             // yellowBox.style.top = '25px';
-//             console.log('you moved forwards');
-//         }
-//         if (keyName === 'a') {
-//             console.log('you moved left');
-//         }
-//         if (keyName === 'd') {
-//             console.log('you moved right');
-//         }
-//         if (keyName === 's') {
-//             console.log('you moved backwards');
-//         }
-//     },
-//     false,
-// );
-
-// const addInput = document.querySelector('#add');
-
-// const removeInput = document.querySelector('#remove');
-
-// const scoreInput = document.querySelector('#score');
-
-// const boardObject = document.querySelector('#board');
-
-// const sensorObject = document.querySelectorAll('.sensor');
-
-// Square Object - needs random evasion mechanism and clicks to points mechanism
-// const squareObject = document.querySelector('.square');
-
-// squareObject.addEventListener('click', function () {
-//     const newColor = makeRandColor();
-//     squareObject.style.backgroundColor = newColor;
-// })
-
-// const clickerObject = document.querySelectorAll('.clicker');
+            console.log('you moved right');
+        }
+        if (keyName === 's') {
+            yellowBox.style.top = (currentTop - moveAmount) + 'px';
+            console.log('you moved backwards');
+        }
+    },
+    false,
+);
